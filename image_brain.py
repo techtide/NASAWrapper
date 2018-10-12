84e2f09b484c6e28070b5ae07aa87b0fd94619d5
Arman B <armanbhalla6@gmail.com>
2018-10-12 21:14:16 +0100

    brain

---
+++ image_brain.py
@@ -0,0 +1,10 @@
+"""
+Created by: Arman B (techtide)
+Purpose: Handles and analyses the data.
+Date: 12/10/2018
+"""
+
+from sklearn.model_selection import train_test_split
+from skimage.feature import hog
+from skimage.io import imread
+from skimage.transform import rescale
