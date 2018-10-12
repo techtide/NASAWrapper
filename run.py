58c40016a60bd66dd1bf8e170a02225e1b2659c0
Arman B <armanbhalla6@gmail.com>
2018-10-12 19:34:34 +0100

    Parsed data stuff

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ import os
 
 API_KEY = ""
 DATA_TYPE = ""
+REQUEST = ""
 
 # Parse through the configuration file. Make sure your config follows the rules and style guide described in README.md.
 with open('config') as input_file:
@@ -19,8 +20,18 @@ with open('config') as input_file:
         if currLine[0] == "data_type":
             DATA_TYPE = currLine[1]
             print("Read data type: " + DATA_TYPE)
+        if currLine[0] == "request":
+            REQUEST = currLine[1]
+            print("Read request url: " + REQUEST)
 
 def get_key():
     return API_KEY
 
+def get_data_type():
+    return DATA_TYPE
+
+def get_datasets():
+    return DATASETS
+
+# Set the environment variable for Bowshock.
 os.environ["NASA_API_KEY"] = API_KEY
