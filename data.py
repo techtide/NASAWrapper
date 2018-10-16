475f578f46b069d3e532f662c812e4dcbe143966
SRugina <30350487+SRugina@users.noreply.github.com>
2018-10-16 22:31:17 +0100

    more or less finished data.py+array

--- data.py
+++ data.py
@@ -12,10 +12,15 @@ import urllib.request
 
 
 
-array1 = [[],[]]
 
 if (get_data_type() == 'image'):
     i = 1
+    #icba to do the array properly
+    array1 = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+    r1 = requests.get(get_request_url())
+    h1 = json.loads(str(r1.text))
+    d1 = h1["date"]
+    dval = int(d1[-2:])
     opznum = 10
     while i <= 20:
         print(get_request_url())
@@ -24,20 +29,36 @@ if (get_data_type() == 'image'):
         h = json.loads(str(r.text))
         d = h["date"]
         opz = ""
-        if i < opznum:
+        if (dval < opznum):
             opz = "0"
-        d1 = d[:-2] + opz + str(i+1)
-        if d1 != d[:-2] + "21":
-            if "hdurl" not in h:
-                print("not img")
-                set_request_url(rurl[:-10] + d1)
-                i = i+1
-                opznum = opznum-1
-                continue
+        else:
+            opz = ""
+        d2 = d[:-2] + opz + str(dval)
+        if ("hdurl" not in h):
+            print("not img")
+            dval = dval+1
+            if (dval < opznum):
+                opz = "0"
             else:
-                imgurl = h["hdurl"]
-                imgnum = i
-                urllib.request.urlretrieve(imgurl, "train/" + str(imgnum) + ".jpg")
-                print(imgurl)
-        set_request_url(rurl[:-10] + d1)
+                opz = ""
+            d2 = d[:-2] + opz + str(dval)
+            set_request_url(rurl[:-10] + d2)
+            continue
+        else:
+            imgurl = h["hdurl"]
+            imgnum = i
+            urllib.request.urlretrieve(imgurl, "train/" + str(imgnum) + ".jpg")
+            array1[imgnum-1][0] = "train/" + str(imgnum) + ".jpg"
+            array1[imgnum-1][1] = "somelabel"
+            array1[imgnum-1][2] = "training"
+            print(imgurl)
+            dval = dval+1
+            if (dval < opznum):
+                opz = "0"
+            else:
+                opz = ""
+            d2 = d[:-2] + opz + str(dval)
+        set_request_url(rurl[:-10] + d2)
         i = i+1
+    if i > 20:
+        print(array1)
