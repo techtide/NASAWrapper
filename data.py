4df9d102ae44017bdaea3499ca6d798533bd7556
SRugina <30350487+SRugina@users.noreply.github.com>
2018-10-19 22:27:49 +0100

    Added image label prompt, added pseudo code for text datatype

--- data.py
+++ data.py
@@ -10,7 +10,21 @@ import requests
 import json
 import urllib.request
 
+import tkinter as tk
+from PIL import ImageTk, Image
+
+def show_imge(path):
+    image_window = tk.Tk()
+    imgr = Image.open(path)
+    imgr = imgr.resize((1280,720), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(imgr)
+    panel = tk.Label(image_window, image=img, height=720, width=1280)
+    panel.pack(side="bottom", fill="both", expand="yes")
+    image_window.mainloop()
+
 array1 = []  # this has the labels
+
+trainortestval = str(get_train_or_test())
 
 if (get_data_type() == 'image'):
     i = 1
@@ -45,22 +59,49 @@ if (get_data_type() == 'image'):
             temparray = [0, 0, 0]
             imgurl = h["hdurl"]
             imgnum = i
-            urllib.request.urlretrieve(imgurl, "train/" + str(imgnum) + ".jpg")
-            temparray[0] = "train/" + str(imgnum) + ".jpg"
-            label_options = get_labels()
-            # TO-DO FOR SEB: SHOW THE IMAGE AND THE DESCRIPTION AND ASK THE USER TO MANUALLY PICK WHICH LABEL CORRESPONDS WITH
-            # THE IMAGE OUT OF THE LABEL OPTIONS
-            temparray[1] = "somelabel"
-            temparray[2] = "training"
-            array1.append(temparray)
-            print(imgurl)
-            dval = dval+1
-            if (dval < opznum):
-                opz = "0"
+            urllib.request.urlretrieve(imgurl, trainortestval + "/" + str(imgnum) + ".jpg")
+            show_imge(trainortestval + "/" + str(imgnum) + ".jpg")
+            print(h["explanation"])
+            labelornot = int(input("Which label should we assign? (0=skip image, 1=" + str(get_labels()[0]) + ", 2=" + str(get_labels()[1]) + ", 3=" + str(get_labels()[2]) + ", 4=" + str(get_labels()[3])))
+            if labelornot != 0:
+                temparray[0] = trainortestval + "/" + str(imgnum) + ".jpg"
+                label_options = get_labels()
+                labelstr = str(get_labels()[labelornot-1])
+                labelstr = labelstr[2:]
+                labelstr = labelstr[:-2]
+                temparray[1] = labelstr
+                temparray[2] = trainortestval
+                array1.append(temparray)
+                print(imgurl)
+                dval = dval+1
+                if (dval < opznum):
+                    opz = "0"
+                else:
+                    opz = ""
+                d2 = d[:-2] + opz + str(dval)
             else:
-                opz = ""
-            d2 = d[:-2] + opz + str(dval)
+                os.remove(trainortestval + "/" + str(imgnum) + ".jpg")
+                dval = dval+1
+                if (dval < opznum):
+                    opz = "0"
+                else:
+                    opz = ""
+                d2 = d[:-2] + opz + str(dval)
+                set_request_url(rurl[:-10] + d2)
+                continue
         set_request_url(rurl[:-10] + d2)
         i = i+1
     if i > 20:
         print(array1)
+elif (get_data_type() == 'text'):
+    #pseudo code
+    r1 = requests.get(get_request2_url())
+    h1 = json.loads(str(r1.text))
+    d1 = h1["diameter"]
+    l1 = h1["type"]
+    temparray = [0, 0, 0]
+    temparray[0] = d1
+    temparray[1] = l1
+    temparray[2] = trainortestval
+    array1.append(temparray)
+    print(array1)
--- run.py
+++ run.py
@@ -10,8 +10,10 @@ import numpy as np
 API_KEY = ""
 DATA_TYPE = ""
 REQUEST = ""
+REQUEST2 = ""
+TRAINORTEST = ""
 
-LABELS = np.array()
+LABELS = []
 
 # Parse through the configuration file. Make sure your config follows the rules and style guide described in README.md.
 with open('config') as input_file:
@@ -27,8 +29,18 @@ with open('config') as input_file:
             REQUEST = currLine[1]
             REQUEST = REQUEST.replace("DEMO_KEY", API_KEY)
             print("Read request url: " + REQUEST)
+        if currLine[0] == "request2":
+            #if a second request is needed
+            REQUEST2 = currLine[1]
+            REQUEST2 = REQUEST2.replace("DEMO_KEY", API_KEY)
+            print("Read request url: " + REQUEST2)
         if "label" in currLine[0]:
-            LABELS.add(currLine[1])
+            templabelarray = [0]
+            templabelarray[0] = currLine[1]
+            LABELS.append(templabelarray)
+        if currLine[0] == "trainortest":
+            TRAINORTEST = currLine[1]
+            print("Read train or test: " + TRAINORTEST)
 
 def get_key():
     return API_KEY
@@ -51,8 +63,22 @@ def set_request_url(val):
     global REQUEST
     REQUEST = val
 
+def get_request2_url():
+    return REQUEST2
+
+def set_request2_url(val):
+    global REQUEST2
+    REQUEST2 = val
+
 def get_labels():
     return LABELS
+
+def get_train_or_test():
+    return TRAINORTEST
+
+def set_train_or_test(val):
+    global TRAINORTEST
+    TRAINORTEST = val
 
 # Set the environment variable for Bowshock.
 os.environ["NASA_API_KEY"] = API_KEY
